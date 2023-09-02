2c49c4eeee911bb218e5e3f1b330482e67e01445
Soumyaraj <soumyarajbag@gmail.com>
2023-09-02 18:57:56 +0530

    Ready to deploy

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ import express from "express";
 import cors from "cors";
 import eventRouter from "./routes/eventRoutes.js";
 import teamRouter from "./routes/teamRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin : "*",
+}));
 app.use("/events" , eventRouter);
 app.use("/team" , teamRouter);
 
+const mongooseUri = process.env.MONGO_URI || "default_fallback_uri"
 mongoose
-  .connect(
-    "mongodb+srv://rcciitd2cigniters:igniter2023@cluster0.e7en8ml.mongodb.net/Igniters?retryWrites=true&w=majority"
-  )
+  .connect(mongooseUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    app.listen(5000);
-  }).then(()=>{
-    console.log("Connected to Database");
-  }).catch((err)=>{
-    console.log(err);
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Your Server is running");
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
   });
